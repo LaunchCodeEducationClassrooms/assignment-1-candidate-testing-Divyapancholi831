86164014baaa5dcf639c07926b2cceda17ef4649
Divyapancholi <DIVYAPANCHOLI1995@GMAIL.COM>
2021-02-16 20:44:17 +0000

    check candidate inputs

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,9 +5,9 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName="";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question="Who was the first american woman in space? ";
+let correctAnswer="Sally Ride";
+let candidateAnswer="";
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -21,13 +21,21 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+candidateAnswer=input.question(question);
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  if(candidateAnswer===correctAnswer)
+  {
+    console.log("Correct Answer");
+  }
+  else
+  {
+    console.log("Incorrect Answer");
+  }
 
 
   let grade;
